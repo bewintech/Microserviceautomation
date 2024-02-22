c0148764218f7a2e39551b7c1a04321a3793fbfe
winfredwamuyu <winnie@localhost.localdomain>
2024-02-22 17:47:40 +0300

    Create New Payment Test

--- src/main/java/org/systech/compliance/pageobjects/paymentPlanManagement/NewPaymentPlan.java
+++ src/main/java/org/systech/compliance/pageobjects/paymentPlanManagement/NewPaymentPlan.java
@@ -1,50 +1,68 @@
-//package org.systech.compliance.pageobjects.paymentPlanManagement;
-//
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.support.FindBy;
-//import org.openqa.selenium.support.PageFactory;
-//import org.systech.compliance.base.BaseClass;
-//
-///**
-// * @author Winfred
-// */
-//public class NewPaymentPlan extends BaseClass {
-//    @FindBy(xpath = "//div[@class='x-treelist-item-text'][contains(.,'Payment Plans Management')]")
-//    WebElement paymentPlansManagement;
-//    @FindBy(xpath = "//a[@class='x-btn x-unselectable x-box-item x-toolbar-item x-btn-default-toolbar-small'][contains(.,'Operations')]")
-//    WebElement operations;
-//    @FindBy(xpath = "//a[@data-ref='itemEl'][contains(.,'Create New')]")
-//    WebElement createNewPlan;
-//    @FindBy(xpath = "//span[@data-ref='btnInnerEl'][contains(.,'Find')]")
-//    WebElement findEmployer;
-//    @FindBy(xpath = "(//div[contains(@class,'x-grid-cell-inner x-grid-checkcolumn-cell-inner')])[6]")
-//    WebElement employer;
-//    @FindBy(xpath = "//span[@data-ref='btnInnerEl'][contains(.,'Apply Selected')]")
-//    WebElement applySelected;
-//    @FindBy(xpath = "//input[contains(@name,'paymentPlan.totalLiability')]")
-//    WebElement totalLiability;
-//    @FindBy(xpath = "//input[contains(@name,'paymentPlan.startDate')]")
-//    WebElement startDate;
-//    @FindBy(xpath = "//input[contains(@name,'paymentPlan.liabilityPeriodFrom')]")
-//    WebElement periodFrom;
-//    @FindBy(xpath = "//input[contains(@name,'paymentPlan.liabilityPeriodTo')]")
-//    WebElement periodTo;
-//    @FindBy(xpath = "//input[contains(@name,'paymentPlan.frequency')]")
-//    WebElement frequency;
-//    @FindBy(xpath = "//input[contains(@name,'paymentPlan.installmentAmount')]")
-//    WebElement installmentAmount;
-//    @FindBy(xpath = "//input[contains(@name,'paymentPlanDetail.paymentType')]")
-//    WebElement paymentType;
-//    @FindBy(xpath = "(//span[contains(@data-ref,'btnIconEl')])[28]")
-//    WebElement searchReceipt;
-//    @FindBy(xpath = "//div[@class='x-grid-cell-inner '][contains(.,'CB001000000008')]")
-//    WebElement receipt;
-//    @FindBy(xpath = "//span[@data-ref='btnInnerEl'][contains(.,'Apply Selected')]")
-//    WebElement applyReceipt;
-//    @FindBy(xpath = "//span[@data-ref='btnInnerEl'][contains(.,'Save')]")
-//    WebElement  save;
-//    public NewPaymentPlan(WebDriver driver){
-//        PageFactory.initElements(driver,this);}
-//    public WebElement getPaymentPlansManagement(){return ehandler.}
-//}
+package org.systech.compliance.pageobjects.paymentPlanManagement;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.systech.compliance.base.BaseClass;
+
+/**
+ * @author Winfred
+ */
+public class NewPaymentPlan extends BaseClass {
+    @FindBy(xpath = "//div[@class='x-treelist-item-text'][contains(.,'Payment Plans Management')]")
+    WebElement paymentPlansManagement;
+    @FindBy(xpath = "//a[@class='x-btn x-unselectable x-box-item x-toolbar-item x-btn-default-toolbar-small'][contains(.,'Operations')]")
+    WebElement operations;
+    @FindBy(xpath = "//a[@data-ref='itemEl'][contains(.,'Create New')]")
+    WebElement createNewPlan;
+    @FindBy(xpath = "//span[@data-ref='btnInnerEl'][contains(.,'Find')]")
+    WebElement findEmployer;
+    @FindBy(xpath = "(//div[contains(.,'Active')])[40]")
+    WebElement employer;
+    @FindBy(xpath = "//span[@data-ref='btnInnerEl'][contains(.,'Apply Selected')]")
+    WebElement applySelected;
+    @FindBy(xpath = "//input[contains(@name,'paymentPlan.totalLiability')]")
+    WebElement totalLiability;
+    @FindBy(xpath = "//input[contains(@name,'paymentPlan.startDate')]")
+    WebElement startDate;
+    @FindBy(xpath = "//input[contains(@name,'paymentPlan.liabilityPeriodFrom')]")
+    WebElement periodFrom;
+    @FindBy(xpath = "//input[contains(@name,'paymentPlan.liabilityPeriodTo')]")
+    WebElement periodTo;
+    @FindBy(xpath = "//input[contains(@name,'paymentPlan.frequency')]")
+    WebElement frequency;
+    @FindBy(xpath = "//input[contains(@name,'paymentPlan.installmentAmount')]")
+    WebElement installmentAmount;
+    @FindBy(xpath = "//input[contains(@name,'paymentPlanDetail.paymentType')]")
+    WebElement paymentType;
+    @FindBy(xpath = "(//span[contains(@data-ref,'btnIconEl')])[28]")
+    WebElement searchReceipt;
+    @FindBy(xpath = "//div[@class='x-grid-cell-inner '][contains(.,'CB001000000008')]")
+    WebElement receipt;
+    @FindBy(xpath = "//span[@data-ref='btnInnerEl'][contains(.,'Apply Selected')]")
+    WebElement applyReceipt;
+    @FindBy(xpath = "//span[@data-ref='btnInnerEl'][contains(.,'Save')]")
+    WebElement  save;
+    public NewPaymentPlan(WebDriver driver){
+        PageFactory.initElements(driver,this);}
+    public WebElement getPaymentPlansManagement(){return ehandler.findClickable(paymentPlansManagement);}
+    public WebElement getOperations(){return ehandler.findClickable(operations);}
+    public WebElement getCreateNewPlan(){return ehandler.findClickable(createNewPlan);}
+    public WebElement getFindEmployer(){return ehandler.findClickable(findEmployer);}
+    public WebElement getEmployer(){return ehandler.findClickable(employer);}
+    public WebElement getApplySelected(){return ehandler.findClickable(applySelected);}
+    public WebElement getTotalLiability(){return ehandler.findClickable(totalLiability);}
+    public WebElement getStartDate(){return ehandler.findClickable(startDate);}
+    public WebElement getPeriodFrom(){return ehandler.findClickable(periodFrom);}
+    public  WebElement getPeriodTo(){return ehandler.findClickable(periodTo);}
+    public WebElement getFrequency(){return ehandler.findClickable(frequency);}
+    public WebElement getInstallmentAmount(){return ehandler.findClickable(installmentAmount);}
+    public WebElement getPaymentType(){return ehandler.findClickable(paymentType);}
+    public WebElement getSearchReceipt(){return ehandler.findClickable(searchReceipt);}
+    public WebElement getReceipt(){return ehandler.findClickable(receipt);}
+    public WebElement getSave(){return ehandler.findClickable(save);}
+    public WebElement getApplyReceipt(){return ehandler.findClickable(applyReceipt);}
+
+
+}
---
+++ src/test/java/Tests/paymentPlanManagementTests/NewPaymentPlanTest.java
@@ -0,0 +1,45 @@
+package Tests.paymentPlanManagementTests;
+
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.systech.compliance.actions.ActionFile;
+import org.systech.compliance.base.BaseClass;
+import org.systech.compliance.pageobjects.paymentPlanManagement.NewPaymentPlan;
+import org.systech.compliance.utils.AssertionMethod;
+import org.testng.annotations.Test;
+
+/**
+ * @author Winfred
+ */
+public class NewPaymentPlanTest extends BaseClass {
+    @Test(priority = 14, description = "Process of Creating a New Payment Plan")
+    @Severity(SeverityLevel.CRITICAL)
+    public void newPaymentPlan() throws InterruptedException{
+        NewPaymentPlan newPaymentPlan = new NewPaymentPlan(driver);
+        ActionFile actions = new ActionFile();
+        newPaymentPlan.getPaymentPlansManagement().click();
+        newPaymentPlan.getOperations().click();
+        newPaymentPlan.getCreateNewPlan().click();
+        newPaymentPlan.getFindEmployer().click();
+        newPaymentPlan.getEmployer().click();
+        newPaymentPlan.getApplySelected().click();
+        newPaymentPlan.getTotalLiability().sendKeys("100000");
+        newPaymentPlan.getStartDate().sendKeys("02/22/2024");
+        newPaymentPlan.getPeriodFrom().sendKeys("11/20/2023");
+        newPaymentPlan.getPeriodTo().sendKeys("11/30/2023");
+        actions.comboDropDown(newPaymentPlan.getFrequency(),"Monthly");
+        newPaymentPlan.getInstallmentAmount().sendKeys("12500");
+        actions.comboDropDown(newPaymentPlan.getPaymentType(),"Penalty");
+        newPaymentPlan.getSearchReceipt().click();
+        newPaymentPlan.getReceipt().click();
+        newPaymentPlan.getApplyReceipt().click();
+        newPaymentPlan.getSave().click();
+
+        String expectedMessage = "Success!";
+        AssertionMethod assertionMethod = new AssertionMethod();
+        assertionMethod.assertToastMessage(driver, expectedMessage);
+
+
+    }
+
+}

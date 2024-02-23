8900fc1068e6f5c849d39344c0d7bc2b160be289
winfredwamuyu <winnie@localhost.localdomain>
2024-02-23 16:26:18 +0300

    Cancel Payment Plan Test

---
+++ src/main/java/org/systech/compliance/pageobjects/paymentPlanManagement/CancelPlan.java
@@ -0,0 +1,25 @@
+package org.systech.compliance.pageobjects.paymentPlanManagement;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.systech.compliance.base.BaseClass;
+
+/**
+ * @author Winfred
+ */
+public class CancelPlan extends BaseClass {
+    @FindBy(xpath = "(//div[contains(@class,'x-grid-cell-inner x-grid-checkcolumn-cell-inner')])[8]")
+    WebElement selectPayment;
+    @FindBy(xpath = "(//span[contains(.,'Cancel Plan')])" )
+    WebElement cancelPlan;
+    @FindBy(xpath = "//span[@data-ref='btnInnerEl'][contains(.,'Yes')]")
+    WebElement confirmCancel;
+    public CancelPlan(WebDriver driver){
+        PageFactory.initElements(driver,this);}
+    public WebElement getCancelPlan(){return ehandler.findClickable(cancelPlan);}
+    public WebElement getConfirmCancel(){return ehandler.findClickable(confirmCancel);}
+    public WebElement getSelectPayment(){return ehandler.findClickable(selectPayment);}
+
+}
---
+++ src/test/java/Tests/paymentPlanManagementTests/CancelPlanTest.java
@@ -0,0 +1,31 @@
+package Tests.paymentPlanManagementTests;
+
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.systech.compliance.base.BaseClass;
+import org.systech.compliance.pageobjects.paymentPlanManagement.CancelPlan;
+import org.systech.compliance.pageobjects.paymentPlanManagement.NewPaymentPlan;
+import org.systech.compliance.utils.AssertionMethod;
+import org.testng.annotations.Test;
+
+/**
+ * @author Winfred
+ */
+public class CancelPlanTest extends BaseClass {
+    @Test(priority = 16, description = "Process of Canceling a Payment Plan")
+    @Severity(SeverityLevel.CRITICAL)
+    public void cancelPlan() throws InterruptedException{
+        NewPaymentPlan newPaymentPlan = new NewPaymentPlan(driver);
+        CancelPlan cancelPlan = new CancelPlan(driver);
+        newPaymentPlan.getPaymentPlansManagement().click();
+        cancelPlan.getSelectPayment().click();
+        newPaymentPlan.getOperations().click();
+        cancelPlan.getCancelPlan().click();
+        cancelPlan.getConfirmCancel().click();
+        String expectedMessage = "Success";
+        AssertionMethod assertionMethod = new AssertionMethod();
+        assertionMethod.assertToastMessage(driver, expectedMessage);
+
+    }
+
+}

eb8e27541cce50e395ddd485fba7ead4e88c9d5b
winfredwamuyu <winnie@localhost.localdomain>
2024-02-26 13:56:47 +0300

    certify and Approve Court Case Test

---
+++ src/test/java/Tests/courtCaseManagementTests/CaseCertificationandApprovalTest.java
@@ -0,0 +1,4 @@
+package Tests.courtCaseManagementTests;/**
+ * @author Winfred
+ */public class CaseCertificationandApprovalTest {
+}
---
+++ src/test/java/Tests/courtCaseManagementTests/RemoveCaseTest.java
@@ -0,0 +1,14 @@
+package Tests.courtCaseManagementTests;
+
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.systech.compliance.base.BaseClass;
+import org.testng.annotations.Test;
+
+/**
+ * @author Winfred
+ */
+public class removeCaseTest extends BaseClass {
+    @Test(priority = 20, description = "Process of Removing a Court Case")
+    @Severity(SeverityLevel.CRITICAL)
+}
